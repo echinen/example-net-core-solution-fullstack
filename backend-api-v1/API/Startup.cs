6cb4977117430a41cf7500f70b5924ecbcd5c1e6
emc-coder <mik.chinen@gmail.com>
2020-07-02 21:28:30 +0800

    new changes

--- backend-api-v1/API/Startup.cs
+++ backend-api-v1/API/Startup.cs
@@ -31,6 +31,11 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -42,8 +47,8 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
+            // app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
